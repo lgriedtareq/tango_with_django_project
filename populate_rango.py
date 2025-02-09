994d6edb223b5281c67d2b600bef078c05884704
Tareq Lgried <2619592L@student.gla.ac.uk>
2025-02-09 22:36:40 +0000

    Chapter 7 Done

--- populate_rango.py
+++ populate_rango.py
@@ -1,7 +1,7 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
-import django
+import django  
 django.setup()
 from rango.models import Category, Page
 
